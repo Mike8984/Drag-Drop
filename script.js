000ad0ffec6c323ebeafc613695c0c8157eb056a
Mike8984 <mikepodgorny08@gmail.com>
2022-12-13 14:04:59 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const item = document.querySelector('.item')
+
+item.addEventListener('dragstart', dragstart)
+item.addEventListener('dragend', dragend)
+
+function dragstart() {}
+
+function dragend() {}
